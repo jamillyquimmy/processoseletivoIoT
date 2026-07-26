93b3a595e6da7ca9c8a9b2468cec94b9ea2dab85
Jamilly <jamilly.quimmy@aluno.ifsertao-pe.edu.br>
2026-07-26 16:17:39 -0300

    Solucao Final: Cenario Light e Relatorio Completo

--- src/main.py
+++ src/main.py
@@ -1,53 +1,34 @@
 import machine
 import time
 
-ldr = machine.ADC(machine.Pin(34))
-ldr.atten(machine.ADC.ATTN_11DB)
-btn = machine.Pin(32, machine.Pin.IN, machine.Pin.PULL_UP)
+PIN_LDR = 34
+PIN_LED = 2
 
-print("Contador de Producao Inicializado")
+ldr = machine.ADC(machine.Pin(PIN_LDR))
+ldr.atten(machine.ADC.ATTN_11V)   
+ldr.width(machine.ADC.WIDTH_12BIT) 
 
-total_pecas = 0
-luz_bloqueada = False
-tempo_inicio_bloqueio = 0
-alerta_emitido = False
+led = machine.Pin(PIN_LED, machine.Pin.OUT)
 
-btn_pressionado = False
-ultimo_clique_btn = 0
+INTERVALO_LEITURA_MS = 100 # Intervalo de leitura
+ultima_leitura = 0
+LIMITE_LUZ = 2000 
+
+print("Iniciando monitoramento Light...")
 
 while True:
-    valor_ldr = ldr.read()
     tempo_atual = time.ticks_ms()
-
-    if valor_ldr < 1000:
-        if not luz_bloqueada:
-
-            luz_bloqueada = True
-            tempo_inicio_bloqueio = tempo_atual
-            alerta_emitido = False
+    
+    if time.ticks_diff(tempo_atual, ultima_leitura) >= INTERVALO_LEITURA_MS:
+        ultima_leitura = tempo_atual
+        
+        valor_luz = ldr.read()
+        
+        if valor_luz > LIMITE_LUZ: 
+            led.value(1) # Liga LED
+            print(f"Luz detectada: Escuro ({valor_luz})") 
         else:
-            if not alerta_emitido and time.ticks_diff(tempo_atual, tempo_inicio_bloqueio) >= 5000:
-                print("Alerta: Micro-parada detectada!")
-                alerta_emitido = True
-                
-    elif valor_ldr > 2000:
-        if luz_bloqueada:
-            # A peça saiu totalmente do sensor
-            total_pecas += 1
-            print(f"Peca detectada! Total: {total_pecas}")
-            luz_bloqueada = False
-            alerta_emitido = False
-
-    leitura_botao = btn.value()
-    if leitura_botao == 0 and not btn_pressionado:
-        if time.ticks_diff(tempo_atual, ultimo_clique_btn) > 200:
-            total_pecas = 0
-            luz_bloqueada = False
-            alerta_emitido = False
-            print("Turno resetado com sucesso. Contadores zerados.")
-            ultimo_clique_btn = tempo_atual
-            btn_pressionado = True
-    elif leitura_botao == 1:
-        btn_pressionado = False
-
+            led.value(0) # Apaga LED
+            print(f"Luz detectada: Claro ({valor_luz})")
+            
     time.sleep_ms(10)
