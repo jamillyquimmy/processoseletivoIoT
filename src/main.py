997e38a320a02471a9bc412f26fd964795594d56
Jamilly <jamilly.quimmy@aluno.ifsertao-pe.edu.br>
2026-07-26 01:01:46 -0300

    Corrigindo logica de micro-parada para atender cenarios 1 e 2

--- src/main.py
+++ src/main.py
@@ -18,24 +18,27 @@ ultimo_clique_btn = 0
 while True:
     valor_ldr = ldr.read()
     tempo_atual = time.ticks_ms()
-    
-    if valor_ldr < 1000 and not luz_bloqueada:
-        luz_bloqueada = True
-        tempo_inicio_bloqueio = tempo_atual
-        alerta_emitido = False
 
-    elif valor_ldr > 2000 and luz_bloqueada:
-        luz_bloqueada = False
-        total_pecas += 1
-        print(f"Peca detectada! Total: {total_pecas}")
+    if valor_ldr < 1000:
+        if not luz_bloqueada:
+            # A peça acabou de entrar
+            luz_bloqueada = True
+            tempo_inicio_bloqueio = tempo_atual
+            alerta_emitido = False
+        else:
+            if not alerta_emitido and time.ticks_diff(tempo_atual, tempo_inicio_bloqueio) >= 5000:
+                print("Alerta: Micro-parada detectada!")
+                alerta_emitido = True
+                
+    elif valor_ldr > 2000:
+        if luz_bloqueada:
+            # A peça saiu totalmente do sensor
+            total_pecas += 1
+            print(f"Peca detectada! Total: {total_pecas}")
+            luz_bloqueada = False
+            alerta_emitido = False
 
-    if luz_bloqueada and not alerta_emitido:
-        if time.ticks_diff(tempo_atual, tempo_inicio_bloqueio) >= 5000:
-            print("Alerta: Micro-parada detectada!")
-            alerta_emitido = True
-            
     leitura_botao = btn.value()
-    
     if leitura_botao == 0 and not btn_pressionado:
         if time.ticks_diff(tempo_atual, ultimo_clique_btn) > 200:
             total_pecas = 0
@@ -43,10 +46,8 @@ while True:
             alerta_emitido = False
             print("Turno resetado com sucesso. Contadores zerados.")
             ultimo_clique_btn = tempo_atual
-            btn_pressionado = True  # TRAVA! Evita repetir a mensagem enquanto estiver segurando
-            
+            btn_pressionado = True
     elif leitura_botao == 1:
-        # O botão foi solto
-        btn_pressionado = False     # DESTRAVA para permitir o próximo clique
+        btn_pressionado = False
 
     time.sleep_ms(10)
